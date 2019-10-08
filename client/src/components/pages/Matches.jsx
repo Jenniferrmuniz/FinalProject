46615dd337760e9b255ea23e887e8f53e502a76b
Jenniferrmuniz <Jennifer.r.muniz@gmail.com>
2019-10-08 10:04:45 -0400

    first commit

---
+++ client/src/components/pages/Matches.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Matches extends Component {
+  render () {
+    return (
+      <div>
+        Matches page!!!
+      </div>
+    )
+  }
+}
+
+export default Matches
---
+++ client/src/components/pages/Quiz.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Quiz extends Component {
+  render () {
+    return (
+      <div>
+        Personallity quiz page!!!
+      </div>
+    )
+  }
+}
+
+export default Quiz
