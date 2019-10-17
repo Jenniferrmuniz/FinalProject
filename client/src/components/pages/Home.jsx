d90acdc9b008a65a5d7f10f88ae4f61be411a706
Jenniferrmuniz <Jennifer.r.muniz@gmail.com>
2019-10-17 10:29:10 -0400

    best match change on hover

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -21,8 +21,8 @@ export default class Home extends Component {
             <div className='bestMatch-div'>
               <p>Your Best Match:</p>
               <div>
-                <Link to={"/all-dogs/" + this.props.topMatch.id}>
-                  <img src={this.props.topMatch.photos[0].small} alt='best matched dog' />
+                <Link className='link-hover-div' to={"/all-dogs/" + this.props.topMatch.id}>
+                  <img className='top-match' src={this.props.topMatch.photos[0].large} alt='best matched dog' />
                   <p>{this.props.topMatch.name}</p>
                 </Link>
               </div>
